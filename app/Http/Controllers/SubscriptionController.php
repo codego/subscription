152bc3c6f789070d4a6f41ef4b1b3040f037348b
user <joacoditoma@gmail.com>
2020-02-13 10:34:20 -0300

    validate user and service

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Subscription;
+use App\User;
+use App\Service;
+
 use Validator;
 
 class SubscriptionController extends Controller
@@ -40,6 +43,9 @@ class SubscriptionController extends Controller
         }
         
         if (!$this->validateExistence($request->user_id, $request->service_id)){
+            User::findOrFail($request->user_id);
+            Service::findOrFail($request->service_id);
+            
             $subscription = new Subscription;
             $subscription->user_id = $request->service_id;
             $subscription->service_id = $request->user_id;
